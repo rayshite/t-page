2a2085fe63a7b24ecd9c2dbe1df68e270d39ec6c
rayshite <rayshite@gmail.com>
2019-03-24 08:35:32 +0300

    install controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,14 @@ require 'uri'
 
 class ApplicationController < ActionController::Base
 	def install
-		url = request.original_url
-		query_params = CGI::parse(URI::parse(url).query)
-    Accounts.create(:shop_id => qyery_params.shop_id, :password => 81831000)
 
-    render 'install'
+		url = request.original_url
+
+		if URI::parse(url).query
+		  query_params = CGI::parse(URI::parse(url).query)
+      Account.create(:shop_id => qyery_params.shop_id, :password => 81831000)
+    end
+
+    render plain: 'install'
   end
 end
